dd275b25b42b0598c75a71082fec575b255c2fbe
M33ks <MeeksFamily06@Outlook.com>
2024-04-14 01:20:42 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Relaxing Stables in the middle of the city">
+    <meta name="keywords" content="Cape Coral horseboarding, SouthEastern Florida horseboarding, horseboarding">
+    <meta name="author" content="Charles Meeks">
+
+    <title>American Cream Stables</title>
+
+    <!--Favicon-->
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+    <!-- Custom CSS -->
+    <link href="css/stables.css?v=2" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+            <a class="navbar-brand" href="#"><em>American Cream Stables<i class="fa-solid fa-horse-head"></i></em></a>
+            </div>
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Image Background Page Header -->
+    <!-- Note: The background image is set within the business-casual.css file. -->
+    <header class="business-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="tagline">Call us today, you won't regret it</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- <hr> -->
+
+        <div class="row">
+            <div class="col-sm-8">
+                <h3>A peaceful resort for your horses</h3>
+
+                <p class="intro">At American Cream Stables, we are focused on horse owners being able to relax in our friendly atmosphere.  The safety and well being of our horses is our prime focus. We have a variety of age, breeds, size, and gender. We have a relaxed group that get together occasionally. Activities include bonfires, obstacle courses, and other social gatherings.</p>
+
+            </div>
+            <div class="col-sm-4">
+            
+                <h3>Give Us A Call!</h3>
+                <strong>We currently have limited spots available </strong>
+                <address>  
+                    16210 Saddlewood Ln<br>
+                    Cape Coral, FL 33991<br>
+                    <a href="https://maps.app.goo.gl/EK6LzzC1ioLZkk6P9" target="_blank">Get Directions</a><br>
+                </address>
+                <address>
+                    <abbr title="Cell Phone">Cell phone:</abbr> <a href="tel:+12399804435">239-980-4435</a><br>
+                </address>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <div class="row">
+            
+            <div class="col-md-6">
+                <h3>Why Choose Us</h3>
+                <ul>
+                    <li>We are a veteran owned stables</li>
+                    <li>Flat areas for riding and training</li>
+                    <li>A pond in the field for the horses to play in</li>
+                    <li>6 stalls</li>
+                    <li>Place for tack and feed</li>
+                    <li>Feed given daily</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+               <img class="img-responsive img-circle img-center" src="img/ridingPen1.jpg" alt="The Riding Pen">
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <h2 class="text-center">Our Services Include</h2>
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive img-center" src="img/horseInField1.jpg" alt="Horse playing in the field">
+                <h2 class="text-center">Fresh Feed and Natural Water Sources</h2>
+                <ul>
+                    <li>We provide hay, which is dispensed daily</li>
+                    <li>Grain provided once a day</li>
+                    <li>Pond in the field</li>
+                    <li>Multiple fields for the horses to roam</li>
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive img-center" src="img/horsesInPond1.jpg" alt="View of the pond">
+                <h2 class="text-center">Beautiful Pastures and Affordable Board</h2>
+                <ul>
+                    <li>15 Acres of pastures with a pond in one</li>
+                    <li>We offer pasture board only</li>    
+                    <li>Monthly board cost for 1 horse is $175 a month</li>
+                    <li>Minimum 1 acre of pasture per horse </li>
+                    <li>6 stall stable for shelter and layups </li>
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive img-center" src="img/washingStall2.jpg" alt="A horse eating">
+                <h2 class="text-center">Specialized Care</h2>
+                <ul>
+                    <li>Horses must be up to date on basic vaccinations, and negative coggins</li>
+                    <li>On site person to care for the horses daily</li>
+                    <li>Grow fod on site</li>
+                    <li>Have stalls for Mini Horses as well</li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="text-center">American Cream Stables &copy; 2024 <br> 16210 Saddlewood Ln, Cape Coral, FL 33991</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
